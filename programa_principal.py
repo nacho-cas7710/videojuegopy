4304bb91c2193be2b7179d8ed8224514760d9619
nacho <ignaciocastillo.3423@gmail.com>
2024-05-10 20:40:20 -0300

    mi juego

---
+++ programa_principal.py
@@ -0,0 +1,22 @@
+
+import suma2
+import resto_funcion
+
+#uilizo la funcion de suma2
+resultado =suma2.suma(3, 5)
+print(resultado) 
+
+
+# Utilizando las funciones de la resto_funcion
+resultado_suma = resto_funcion.suma(10, 5)
+resultado_resta = resto_funcion.resta(10, 5)
+resultado_multiplicacion = resto_funcion.multiplicacion(10, 5)
+resultado_division = resto_funcion.division(10, 5)
+
+print("Suma:", resultado_suma)  # Output: 15
+print("Resta:", resultado_resta)  # Output: 5
+print("Multiplicación:", resultado_multiplicacion)  # Output: 50
+print("División:", resultado_division)  # Output: 2.0
+
+
+
---
+++ resto_funcion.py
@@ -0,0 +1,16 @@
+# calculadora.py
+def suma(a, b):
+    return a + b
+
+def resta(a, b):
+    return a - b
+
+def multiplicacion(a, b):
+    return a * b
+
+def division(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "No se puede dividir entre cero"
+print("hola")
